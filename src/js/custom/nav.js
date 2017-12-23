7b1427ff149bfadeb299ec282d440a892a403e8f
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2017-12-23 16:18:28 +0100

    feat(add smooth scrolling and show actual section on scroll):

--- src/js/custom/nav.js
+++ src/js/custom/nav.js
@@ -4,5 +4,38 @@
             $(".nav__item--active").removeClass("nav__item--active");
             $(this).parent().addClass("nav__item--active");
         });
+
+        // show actual section
+        $(document).on("scroll", changeNavActiveOnScroll);
+
+        // smoothscroll
+        $(".nav .nav__item a").click(function(event) {
+            if (this.hash !== "") {
+                console.log(this.hash);
+                event.preventDefault();
+
+                var hash = this.hash;
+                var pos = $(hash).offset().top
+
+                $("html,body").animate({scrollTop: pos}, 800);
+            }
+        });
     });
 })($);
+
+
+
+function changeNavActiveOnScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.nav .nav__item a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav .nav__item').removeClass("nav__item--active");
+            $(this).parent().addClass("nav__item--active");
+        }
+        else {
+            currLink.removeClass("nav__item--active");
+        }
+    });
+}
